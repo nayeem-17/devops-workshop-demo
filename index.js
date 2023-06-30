ee34f091c3c618853011f67a304bdc4d391ef32f
nayeem-17 <nasa.28apr@gmail.com>
2023-06-30 12:48:34 +0600

    made changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/hello', (req, res) => {
-    res.send('Hello, world!');
+    res.send('Hello, world! This is a test');
 });
 
 app.get('/bye', (req, res) => {
